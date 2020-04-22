a3b3195dcb101b14b606d22d1ab86eaa3fe0fb27
冯国清 <master@rebeta.cn>
2020-04-22 09:27:35 +0800

    feat : complete the errors;

---
+++ errors.go
@@ -0,0 +1,187 @@
+/*
+   @Time : 2020/4/20 3:57 下午
+   @Author : Rebeta
+   @Email : master@rebeta.cn
+   @File : errors
+   @Software: GoLand
+*/
+
+package chaos
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// ErrorType 是在 gin 规范中定义的无符号64位错误代码
+// ErrorType is an unsigned 64-bit error code as defined in the gin spec.
+type ErrorType uint64
+
+const (
+	// ErrorTypeBind 会在 Context.Bind() 失败时被使用
+	// ErrorTypeBind is used when Context.Bind() fails.
+	ErrorTypeBind ErrorType = 1 << 63
+	// ErrorTypePrivate 表示私有错误
+	// ErrorTypePrivate indicates a private error.
+	ErrorTypePrivate ErrorType = 1 << 0
+	// ErrorTypePublic 表示公共错误
+	// ErrorTypePublic indicates a public error.
+	ErrorTypePublic ErrorType = 1 << 1
+	// ErrorTypeAny 表示所有其他错误
+	// ErrorTypeAny indicates any other error.
+	ErrorTypeAny ErrorType = 1<<64 - 1
+)
+
+// Error 用来规定一个 error 的规范
+// Error represents a error's specification.
+type Error struct {
+	Err  error
+	Type ErrorType
+	Meta interface{}
+}
+
+type errorMsgs []*Error
+
+var _ error = &Error{}
+
+// SetType 用来设置错误的类型
+// SetType sets the error's type.
+func (msg *Error) SetType(flags ErrorType) *Error {
+	msg.Type = flags
+	return msg
+}
+
+// SetMeta 用来设置错误的元数据
+// SetMeta sets the error's meta data.
+func (msg *Error) SetMeta(data interface{}) *Error {
+	msg.Meta = data
+	return msg
+}
+
+// JSON 用来创建格式正确的 JSON
+// JSON creates a properly formatted JSON
+func (msg *Error) JSON() interface{} {
+	json := H{}
+	if msg.Meta != nil {
+		value := reflect.ValueOf(msg.Meta)
+		switch value.Kind() {
+		case reflect.Struct:
+			return msg.Meta
+		case reflect.Map:
+			for _, key := range value.MapKeys() {
+				json[key.String()] = value.MapIndex(key).Interface()
+			}
+		default:
+			json["meta"] = msg.Meta
+		}
+	}
+	if _, ok := json["error"]; !ok {
+		json["error"] = msg.Error()
+	}
+	return json
+}
+
+// MarshalJSON 用来实现 json.Marshaller 接口
+// MarshalJSON implements the json.Marshaller interface.
+func (msg *Error) MarshalJSON() ([]byte, error) {
+	return json.Marshal(msg.JSON())
+}
+
+// Error 实现 error 的接口
+// Error implements the error interface.
+func (msg Error) Error() string {
+	return msg.Err.Error()
+}
+
+// IsType 用来推断一个错误的类型
+// IsType judges one error.
+func (msg *Error) IsType(flags ErrorType) bool {
+	return (msg.Type & flags) > 0
+}
+
+// ByType 返回经过过滤 byte 的制度只读副本
+// 即 ByType(gin.ErrorTypePublic) 返回一段类型为 ErrorTypePublic 的错误
+// ByType returns a readonly copy filtered the byte.
+// ie ByType(gin.ErrorTypePublic) returns a slice of errors with type=ErrorTypePublic.
+func (a errorMsgs) ByType(typ ErrorType) errorMsgs {
+	if len(a) == 0 {
+		return nil
+	}
+	if typ == ErrorTypeAny {
+		return a
+	}
+	var result errorMsgs
+	for _, msg := range a {
+		if msg.IsType(typ) {
+			result = append(result, msg)
+		}
+	}
+	return result
+}
+
+// Last 返回切片中的最后一个错误。 如果数组为空，则返回nil
+// errors[len(errors)-1] 的语法糖
+// Last returns the last error in the slice. It returns nil if the array is empty.
+// Shortcut for errors[len(errors)-1].
+func (a errorMsgs) Last() *Error {
+	if length := len(a); length > 0 {
+		return a[length-1]
+	}
+	return nil
+}
+
+// Errors returns an array will all the error messages.
+// Errors 返回一个包含所有错误的数组
+// 例如:
+// Example:
+// 		c.Error(errors.New("first"))
+// 		c.Error(errors.New("second"))
+// 		c.Error(errors.New("third"))
+// 		c.Errors.Errors() // == []string{"first", "second", "third"}
+func (a errorMsgs) Errors() []string {
+	if len(a) == 0 {
+		return nil
+	}
+	errorStrings := make([]string, len(a))
+	for i, err := range a {
+		errorStrings[i] = err.Error()
+	}
+	return errorStrings
+}
+
+func (a errorMsgs) JSON() interface{} {
+	switch len(a) {
+	case 0:
+		return nil
+	case 1:
+		return a.Last().JSON()
+	default:
+		json := make([]interface{}, len(a))
+		for i, err := range a {
+			json[i] = err.JSON()
+		}
+		return json
+	}
+}
+
+// MarshalJSON 实现 json.Marshaller 接口。可以移除最终返回的 json 最外层 [] 的效果
+// MarshalJSON implements the json.Marshaller interface.
+func (a errorMsgs) MarshalJSON() ([]byte, error) {
+	return json.Marshal(a.JSON())
+}
+
+func (a errorMsgs) String() string {
+	if len(a) == 0 {
+		return ""
+	}
+	var buffer strings.Builder
+	for i, msg := range a {
+		fmt.Fprintf(&buffer, "Error #%02d: %s\n", i+1, msg.Err)
+		if msg.Meta != nil {
+			fmt.Fprintf(&buffer, "     Meta: %v\n", msg.Meta)
+		}
+	}
+	return buffer.String()
+}
